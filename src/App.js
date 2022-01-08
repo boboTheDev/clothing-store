c63a5f51ea5d3f3d6a0b783891c93079aac223f5
boboTheDev <minnbobo.official@gmail.com>
2022-01-08 12:49:53 +0630

    added firebase utils, including user to store in firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.page";
 import ShopPage from "./pages/shop/shop.page";
 import SignInUp from "./pages/sign-in-up/sign-in-up.page";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +18,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // this.setState({ currentUser: userAuth });
+      // createUserProfileDocument(userAuth);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-up/sign-in-up.page.jsx
+++ src/pages/sign-in-up/sign-in-up.page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./sign-in-up.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.components";
-
+import SignUp from "../../components/sign-up/sign-up.components";
 const SignInUp = () => (
   <div className="sign-in-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
