367e6195aad1fe2a1991567e4428528fe5d95617
boboTheDev <minnbobo.official@gmail.com>
2022-01-23 23:12:47 +0630

    added persist

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -24,8 +24,7 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
         <div className="arrow" onClick={() => removeItem(cartItem)}>
           &#10094;
         </div>
-        <span className="value"></span>
-        {quantity}
+        <span className="value">{quantity}</span>
         <div className="arrow" onClick={() => addItem(cartItem)}>
           &#10095;
         </div>
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+//note: storage is from the actual local storage
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+//note: user is being handled by firebase auth
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
